7856a23bc29f5d90abcab1401f1e9de1938d1a1c
GUILLAUME DE CRAENE <guillaume.decraene@student.hogent.be>
2023-04-17 13:46:19 +0200

    begin

--- src/main/java/com/project/ewdj/EwdjApplication.java
+++ src/main/java/com/project/ewdj/EwdjApplication.java
@@ -2,12 +2,28 @@ package com.project.ewdj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import domein.AlbumExpertBean;
+import domein.ExpertBean;
 
 @SpringBootApplication
-public class EwdjApplication {
+public class EwdjApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EwdjApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/welcome");
+	}
+
+	@Bean
+	ExpertBean expertBean() {
+		return new AlbumExpertBean();
 	}
 
 }
---
+++ src/main/java/com/project/ewdj/WelcomeCommand.java
@@ -0,0 +1,11 @@
+package com.project.ewdj;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WelcomeCommand {
+
+    private String albumSelected;
+}
