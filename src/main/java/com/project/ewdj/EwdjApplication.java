97f6441c8ab2926a70dd47ea9533609b1aa7380d
GUILLAUME DE CRAENE <guillaume.decraene@student.hogent.be>
2023-04-17 12:59:35 +0200

    first commit

---
+++ src/main/java/com/project/ewdj/EwdjApplication.java
@@ -0,0 +1,13 @@
+package com.project.ewdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ewdj/EwdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ewdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
